0ba8faa771da52e35eae978c007b07581408a23b
John F Kidd <john_kidd@outlook.com>
2016-11-05 15:48:45 +0000

    more puzzles added

---
+++ fsharp/add/Program.fs
@@ -0,0 +1,13 @@
+﻿open System
+
+let add a b = a + b;
+
+[<EntryPoint>]
+let main argv = 
+    let a = Console.ReadLine() |> int
+    let b = Console.ReadLine() |> int
+    let c = add a b;
+    printfn "%d" c;
+
+    0 // return an integer exit code
+
