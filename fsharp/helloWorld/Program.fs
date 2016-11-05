1494c3a115f820784bf0666714468e0305f43abb
John F Kidd <john_kidd@outlook.com>
2016-11-05 15:52:00 +0000

    simple f# starter puzzles

---
+++ fsharp/helloWorld/Program.fs
@@ -0,0 +1,15 @@
+﻿open System
+
+let hello_worlds n = 
+    for i in 1 .. n do
+        Console.WriteLine "Hello World" 
+
+let add a b = a + b;
+
+[<EntryPoint>]
+let main argv = 
+    let tokens_n = Console.ReadLine();
+    let n = Convert.ToInt32(tokens_n);
+    hello_worlds n;
+    0 // return an integer exit code
+
