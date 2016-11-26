80983a60f110d7384313c3a42d6f60a60beed6d1
John F Kidd <john_kidd@outlook.com>
2016-11-26 17:41:56 +0000

    added get max stop profit

---
+++ csharp/:Users:johnkidd:Projects:puzzles:csharp:Puzzles:YesterdaysStock.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace Puzzles
+{
+	public static class YesterdaysStock
+	{
+		public static int CalculateMaxReturn(int[] stockPricesYesterday)
+		{
+			var min = 0;
+			var max = 0;
+
+			foreach (var stockPrice in stockPricesYesterday)
+			{
+				if (stockPrice > max)
+					max = stockPrice;
+
+				if (min == 0 || stockPrice < min)
+					min = stockPrice;
+
+			}
+
+			return max - min;
+		}
+	}
+}
---
+++ csharp/Puzzles.Specs/YesterdaysStockTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Puzzles.Specs
+{
+	[TestFixture]
+	public class YesterdaysStockTests
+	{
+		[Test]
+		public void CalculateMaxReturn_ReturnsExpectedValue()
+		{
+			// arrange 
+			var input = new[] { 10, 7, 5, 8, 11, 9 };
+
+			// act
+			var actual = YesterdaysStock.CalculateMaxReturn(input);
+
+			// assert
+			Assert.That(6 == actual);
+		}	
+	}
+}
